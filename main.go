2acec670144af89f3656ae95f7133fc7e1b1fbce
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-29 12:42:03 +0200

    Implement database connection functionality and update main function
    Co-authored-by: Fayssal Mechmeche <fayssalmechmeche@users.noreply.github.com>

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"postgo/functions"
 
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	db, err := functions.Connect("localhost", 5432, "postgo", "postgo", "postgo")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+	fmt.Println("Connected to the database successfully!")
 }
