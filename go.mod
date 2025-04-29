187029b7bc49f6abc38e30b7a054a3e1fab4e7e4
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-04-29 12:04:58 +0200

    Install pq and create sample main function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module postgo
 
 go 1.23.6
+
+require github.com/lib/pq v1.10.9 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
