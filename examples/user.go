a76830ef38ace3d9c45206210d89fa73dc023df8
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-05-27 10:15:07 +0200

    Fix indent

--- examples/user.go
+++ examples/user.go
@@ -5,12 +5,12 @@ import (
 )
 
 type User struct {
-    db.BaseModel
-    Name     string `db:"not_null"`
-    Email    string `db:"not_null,unique"`
-    Password string `db:"not_null"`
+	db.BaseModel
+	Name     string `db:"not_null"`
+	Email    string `db:"not_null,unique"`
+	Password string `db:"not_null"`
 }
 
 func (u *User) TableName() string {
-    return "users"
+	return "users"
 }
